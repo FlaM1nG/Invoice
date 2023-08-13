fdbcd1d301517b460c1a1be627bcd31d82653979
flam1ng <carlosamg87@gmail.com>
2023-08-13 17:28:33 +0200

    refactor(appstepper): styles refactor

--- src/components/AppStepper.vue
+++ src/components/AppStepper.vue
@@ -435,11 +435,11 @@ const upload = async (): Promise<void> => {
 </script>
   
 <style lang="scss" scoped>
-$default    :   #C5C5C5;
-$green-1    :   #75CC65;
-$transiton  :   all 500ms ease;
+$default:#C5C5C5;
+$green-1:#75CC65;
+$transiton:all 500ms ease;
 
-body{
+body {
   background-image: linear-gradient(60deg, #abecd6 0%, #fbed96 100%);
   color: #ffffff;
   width: 100%;
@@ -450,9 +450,9 @@ body{
   font-family: sans-serif;
 }
 
-.tx-green-1{
-    color:  $green-1;
-    font-weight: 600;
+.tx-green-1 {
+  color:  $green-1;
+  font-weight: 600;
 }
 
 .validation-message {
@@ -462,125 +462,124 @@ body{
 }
 
 .wrapper-stepper{
-    background-color: #fff;
-    padding: 60px;
-    border-radius: 32px;
-    box-shadow: rgba($color: #000000, $alpha: 0.09);
+  background-color: #fff;
+  padding: 60px;
+  border-radius: 32px;
+  box-shadow: rgba($color: #000000, $alpha: 0.09);
 }
 
 .stepper{
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 660px;
-    position: relative;
-    z-index: 0;
-    margin-bottom: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 660px;
+  position: relative;
+  z-index: 0;
+  margin-bottom: 24px;
 
-    &-progress{
+  &-progress{
+    position: absolute;
+    background-color: $default;
+    height: 2px;
+    z-index: -1;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+
+    &-bar{
         position: absolute;
-        background-color: $default;
-        height: 2px;
-        z-index: -1;
         left: 0;
-        right: 0;
-        margin: 0 auto;
-
-        &-bar{
-            position: absolute;
-            left: 0;
-            height: 100%;
-            width: 0%;
-            background-color: $green-1;
-            transition: $transiton;
-        }
+        height: 100%;
+        width: 0%;
+        background-color: $green-1;
+        transition: $transiton;
     }
+  }
 }
 
 .stepper-item{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    color: $default;
-    transition: $transiton;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: $default;
+  transition: $transiton;
 
-    &-counter{
-        height: 68px;
-        width: 68px;
-        display: grid;
-        place-items: center;
-        background-color: #fff;
-        border-radius: 100%;
-        border: 2px solid $default;
-        position: relative;
+  &-counter{
+    height: 68px;
+    width: 68px;
+    display: grid;
+    place-items: center;
+    background-color: #fff;
+    border-radius: 100%;
+    border: 2px solid $default;
+    position: relative;
 
-        .icon-success{
-            position: absolute;
-            opacity: 0;
-            transform: scale(0);
-            width: 24px;
-            transition: $transiton;
-        }
-
-        .number{
-            font-size: 22px;
-            transition: $transiton;
-        }
+    .icon-success{
+      position: absolute;
+      opacity: 0;
+      transform: scale(0);
+      width: 24px;
+      transition: $transiton;
     }
 
-    &-title{
-        position: absolute;
-        font-size: 14px;
-        bottom: -24px;
+    .number{
+      font-size: 22px;
+      transition: $transiton;
     }
+  }
+
+  &-title{
+    position: absolute;
+    font-size: 14px;
+    bottom: -24px;
+  }
 }
 
-.stepper-item.success{
-    .stepper-item-counter{
-        border-color: $green-1;
-        background-color: #c8ebc1;
-        color: #fff;
-        font-weight: 600;
-
-        .icon-success{
-            opacity: 1;
-            transform: scale(1);
-        }
-
-        .number{
-            opacity: 0;
-            transform: scale(0);
-        }
+.stepper-item.success {
+  .stepper-item-counter{
+    border-color: $green-1;
+    background-color: #c8ebc1;
+    color: #fff;
+    font-weight: 600;
+    .icon-success{
+        opacity: 1;
+        transform: scale(1);
     }
 
-    .stepper-item-title{
-        color: $green-1;
+    .number{
+        opacity: 0;
+        transform: scale(0);
     }
+  }
+
+  .stepper-item-title{
+    color: $green-1;
+  }
 }
 
 .stepper-item.current{
-    .stepper-item-counter{
-        border-color: $green-1;
-        background-color: $green-1;
-        color: #fff;
-        font-weight: 600;
-    }
+  .stepper-item-counter{
+      border-color: $green-1;
+      background-color: $green-1;
+      color: #fff;
+      font-weight: 600;
+  }
 
-    .stepper-item-title{
-        color: #818181;
-    }
+  .stepper-item-title{
+      color: #818181;
+  }
 }
 
 .stepper-pane{
-    color: #333;
-    text-align: center;
-    height: 400px;
-    box-shadow: 0 8px 12px rgba($color: #000000, $alpha: 0.09);
-    margin: 40px 0;
+  color: #333;
+  text-align: center;
+  height: 400px;
+  box-shadow: 0 8px 12px rgba($color: #000000, $alpha: 0.09);
+  margin: 40px 0;
 }
 
 .controls{
-    display: flex;
+  display: flex;
 }
 
 /* inputs */
@@ -620,10 +619,9 @@ body{
   background-color: #fff;
   padding-inline: 5px;
 }
-
 .input-group__input[type="date"] + .input-group__label {
   transform: translate(-5px, -18px) scale(0.8);
   background-color: #fff;
   padding-inline: 5px;
 }
-  </style>
+</style>
