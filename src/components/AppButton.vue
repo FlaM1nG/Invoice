a81e1297bb3e19a68537dcfd311478da6ee9543b
flam1ng <carlosamg87@gmail.com>
2023-08-13 13:30:14 +0200

    refactor(appbutton): add types and refactor function app button

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,7 +1,7 @@
 <template>
     <button 
         :disabled="props.isDisabled"
-        :class="[props.isDisabled ? 'cursor-not-allowed': 'cursor-pointer', bgColorText ? bgColorText : 'bg-transparent']" 
+        :class="buttonClass" 
         class="btn"
         @click="buttonClick"
         >
@@ -11,58 +11,70 @@
 
 <script setup lang="ts">
 
-const props = defineProps({
-  isDisabled: {
-    type: Boolean,
-    default: false,
-  },
+import { computed } from 'vue';
+
+interface ButtonProps {
+  isDisabled: boolean;
+  bgColorText: string;
+}
+
+const props: ButtonProps = defineProps({
+  isDisabled: Boolean,
   bgColorText: {
     type: String,
     default: '',
   },
 });
 
+const buttonClass = computed<Record<string, boolean>>(() => {
+  return {
+    'cursor-not-allowed': props.isDisabled,
+    'cursor-pointer': !props.isDisabled,
+    [props.bgColorText || 'bg-transparent']: true,
+    'btn': true
+  };
+});
+
 const emit = defineEmits(['custom-click'])
 
-const buttonClick = () => {
-    emit('custom-click')
-  }
+const buttonClick: () => void = () => {
+  emit('custom-click');
+};
 
 </script>
-
 <style lang="scss">
 $green-1: #75CC65;
-.btn{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 6px 16px;
-    border: 1px solid;
-    text-align: center;
-    vertical-align: middle;
-    line-height: 1.5;
-    transition: all 150ms;
-    border-radius: 4px;
-    width: fit-content;
-    font-size: 0.75rem;
-    color: #333;
-    background-color: #f0f0f0;
-    border-color: #f0f0f0;
+.btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 6px 16px;
+  border: 1px solid;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 1.5;
+  transition: all 150ms;
+  border-radius: 4px;
+  width: fit-content;
+  font-size: 0.75rem;
+  color: #333;
+  background-color: #f0f0f0;
+  border-color: #f0f0f0;
 
-    &:disabled{
-      opacity: 0.5;
-    }
+  &:disabled{
+    opacity: 0.5;
+  }
 
-    &--green{
-      background-color: $green-1;
-      border-color: $green-1;
-      color: #fff;
-      &-1 {
-      background-color: $green-1;
-      border-color: $green-1;
-      color: #fff;
-        margin-left: auto;
-      }
+  &--green{
+    background-color: $green-1;
+    border-color: $green-1;
+    color: #fff;
+    &-1 {
+    background-color: $green-1;
+    border-color: $green-1;
+    color: #fff;
+      margin-left: auto;
     }
+  }
 }
 </style>
