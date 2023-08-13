de5fa0655fd45d382f8fafc59c3bd7399cf8cfe8
flam1ng <carlosamg87@gmail.com>
2023-08-13 12:12:56 +0200

    feat(index): router

---
+++ src/router/index.ts
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
+import ErrorView from '../views/ErrorView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: LoginView,
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'), // Lazy loading
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "error",
+    component: ErrorView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+// Middleware to check authentication before each navigation
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.name !== 'login';
+  const isAuthenticated = !!sessionStorage.getItem('authCode');
+
+  if (requiresAuth && !isAuthenticated) {
+    next({ name: 'login' });
+  } else {
+    next();
+  }
+});
+
+export default router;
