f3156eabc3740981f62af9eefb6c180fac2da80f
flam1ng <carlosamg87@gmail.com>
2023-08-13 12:14:03 +0200

    test(appbutton): test button component

---
+++ src/components/__tests__/AppButton.spec.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import AppButton from '../AppButton.vue'
+
+describe('AppButton', () => {
+    it('renders button correctly', () => {
+      const wrapper = mount(AppButton)
+      expect(wrapper.exists()).toBe(true)
+    })
+    it('emits custom-click event when clicked', async () => {
+      const wrapper = mount(AppButton)
+      await wrapper.trigger('click')
+      expect(wrapper.emitted()['custom-click']).toBeTruthy()
+    })
+    it('disables button when isDisabled prop is true', () => {
+      const wrapper = mount(AppButton, {
+        props: {
+          isDisabled: true,
+        },
+      })
+      expect(wrapper.find('button').element.disabled).toBe(true)
+    })
+    it('applies custom background color class when bgColorText prop is provided', () => {
+      const bgColor = 'bg-custom-color'
+      const wrapper = mount(AppButton, {
+        props: {
+          bgColorText: bgColor,
+        },
+      })
+      expect(wrapper.find('button').classes()).toContain(bgColor)
+    })
+})
