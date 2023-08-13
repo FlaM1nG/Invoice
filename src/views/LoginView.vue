ff87cc2ab7853876c7de758a1711199219ad79a0
flam1ng <carlosamg87@gmail.com>
2023-08-13 15:17:39 +0200

    fix(loginview): add tailwind class

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container-login100">
+  <div class="container-login w-full flex flex-grow flex-wrap justify-center items-center">
   <div class="wrap-login">
-    <div class="flex justify-center text-sm py-4 title-login">Sign In</div>
+    <div class="flex justify-center text-sm py-4 w-full title-login">Sign In</div>
     <div class="flex justify-center">
       <GoogleLogin :callback="callback">
         <AppButton>
@@ -21,44 +21,36 @@ import AppButton from "@/components/AppButton.vue";
 const router = useRouter();
 
 const callback: CallbackTypes.CodeResponseCallback = (response) => {
-  console.log("Authorisation code", response)
-  // Save in the Local Storage the code
+  // Save in the Session Storage the code
   sessionStorage.setItem('authCode', response.code)
   router.push({ name: 'home' })
 };
 </script>
 
 <style scoped>
-.container-login100 {
-  width: 100%;
-    display: flex;
-    flex-grow: 1;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    background-image: linear-gradient(60deg, #abecd6 0%, #fbed96 100%);
+.container-login{
+  background-image: linear-gradient(60deg, #abecd6 0%, #fbed96 100%);
 }
 
 .wrap-login {
-    height: 125px;
-    width: 270px;
-    background: #fff;
-    border-radius: 10px;
-    overflow: hidden;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -moz-box;
-    display: -ms-flexbox;
+  height: 125px;
+  width: 270px;
+  background: #fff;
+  border-radius: 10px;
+  overflow: hidden;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -moz-box;
+  display: -ms-flexbox;
 }
 
 .title-login {
-    font-family: Poppins-Bold;
-    font-size: 24px;
-    font-weight: bold;
-    color: #333;
-    line-height: 1.2;
-    text-align: center;
-    width: 100%;
-    display: block;
+  font-family: Poppins-Bold;
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+  line-height: 1.2;
+  text-align: center;
+  display: block;
 }
 </style>
