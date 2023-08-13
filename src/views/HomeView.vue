21c5da12e2ae7d40ba3887d04cda6a8a8db47834
flam1ng <carlosamg87@gmail.com>
2023-08-13 23:33:08 +0200

    feat(homeview): change view for button new invoice

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
-  <AppStepper/>
+  <AppButton @custom-click="navigateToInvoice">
+    New Invoice
+  </AppButton>
 </template>
     
 <script setup lang="ts">
-import AppStepper from '@/components/AppStepper.vue';
+import { useRouter } from 'vue-router';
+import AppButton from '@/components/AppButton.vue';
+
+const router = useRouter();
+
+const navigateToInvoice = (): void => {
+  router.push({ name: 'invoice' }); 
+};
 </script>
