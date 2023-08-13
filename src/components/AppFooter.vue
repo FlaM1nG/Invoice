7742938045c37e5cadb496934393ade0d596304a
flam1ng <carlosamg87@gmail.com>
2023-08-13 12:10:31 +0200

    feat(appfooter): footer component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="w-full footer text-black font-xs font-mono font-normal items-center inline-flex justify-center h-10 text-xs">
+        Invoice
+        Copyright © {{currentYear }} · All Rights Reserved
+    </footer> 
+ </template>
+ 
+ <script setup lang="ts">
+ import { computed } from 'vue';
+
+const date = new Date();
+const currentYear = computed(() => date.getFullYear());
+ 
+ </script>
+
+<style scoped>
+.footer {
+    width: 100%;
+    background: #9053c7;;
+    background-image: linear-gradient(60deg, #abecd6 0%, #fbed96 100%);
+}
+</style>
