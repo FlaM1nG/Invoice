edef8eb65489779d61ed950e694ceb9aaffb3d2e
flam1ng <carlosamg87@gmail.com>
2023-08-13 12:16:26 +0200

    feat(error): error view

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex-col container mx-auto px-5 sm:px-20 py-20 flex justify-center">
+      <h1 class="text-gray-700 text-6xl text-center"> Page not found.</h1>
+      <p class="text-gray-600 text-8xl text-center mt-8">
+        Error 404
+      </p>
+      <router-link
+        to="/home"
+        class="mt-5 text-xl text-green-600 hover:underline text-center">
+          Back to the home
+      </router-link>
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
