1a10fdf687f190dd02a6abcdc960715e3bd0b3be
flam1ng <carlosamg87@gmail.com>
2023-08-13 14:46:02 +0200

    fix(aboutview): remove about view and router

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,11 +15,6 @@ const routes = [
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue'), // Lazy loading
-  },
-  {
     path: "/:catchAll(.*)",
     name: "error",
     component: ErrorView,
