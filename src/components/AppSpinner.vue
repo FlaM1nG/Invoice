0f1e0aeb8c431dfe66eb342ef983beb077055f09
flam1ng <carlosamg87@gmail.com>
2023-08-13 18:15:49 +0200

    feat(appspinner): new component spinner

---
+++ src/components/AppSpinner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-loading">
+  </div>
+</template>
+  
+<style scoped>
+.container-loading {
+  width: 35px;
+  height: 35px;
+  display: inline-block;
+  border: 5px solid rgb(20 195 20 / 50%);
+  border-right-color: rgb(130 255 130 / 80%);
+  border-bottom-color: rgb(130 255 130 / 80%);
+  border-radius: 50%;
+  animation: rotate 600ms infinite linear;
+}
+
+@keyframes rotate {
+  to {
+    transform: rotate(1turn)
+  }
+}
+</style>
