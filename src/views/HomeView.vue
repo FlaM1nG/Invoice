cd28b1f914fd1be27b9c0c5a10c329bd8e529788
flam1ng <carlosamg87@gmail.com>
2023-08-13 12:17:42 +0200

    feat(home): home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container-login100">
+        <div class="wrap-login">
+            <AppStepper/>
+        </div>
+    </div>
+</template>
+    
+<script setup lang="ts">
+import AppStepper from '@/components/AppStepper.vue';
+</script>
+
+<style scoped></style>
