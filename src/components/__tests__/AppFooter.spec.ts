565e1cb8f06046636698e8594837240516915ddd
flam1ng <carlosamg87@gmail.com>
2023-08-14 21:39:45 +0200

    test(appfooter): refactor test

--- src/components/__tests__/AppFooter.spec.ts
+++ src/components/__tests__/AppFooter.spec.ts
@@ -1,31 +1,31 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import AppFooter from '@/components/AppFooter.vue';
 
-import { mount } from '@vue/test-utils'
-import AppFooter from '@/components/AppFooter.vue'
+describe('Footer', () => {
+  it('renders correctly', () => {
+    const wrapper = mount(AppFooter, {
+      global: {
+        mocks: {
+          $t: (key: any) => key, 
+        },
+      },
+    });
 
-describe('AppFooter', () => {
-  it('renders current year correctly', () => {
-    const wrapper = mount(AppFooter);
+    expect(wrapper.find('footer').exists()).toBe(true);
+    expect(wrapper.find('.bg-gradient-to-r').exists()).toBe(true);
+  });
+
+  it('renders current year', () => {
+    const wrapper = mount(AppFooter, {
+      global: {
+        mocks: {
+          $t: (key:any) => key,
+        },
+      },
+    });
+
     const currentYear = new Date().getFullYear();
-    expect(wrapper.text()).toContain(currentYear);
-  });
-
-  it('renders the "Invoice" text', () => {
-    const wrapper = mount(AppFooter);
-    expect(wrapper.text()).toContain('Invoice');
-  });
-
-  it('has a valid copyright notice', () => {
-    const wrapper = mount(AppFooter);
-    const currentYear = new Date().getFullYear();
-    const copyrightNotice = `Copyright © ${currentYear} · All Rights Reserved`;
-    expect(wrapper.text()).toContain(copyrightNotice);
-  });
-
-  it('has the correct font and font size', () => {
-    const wrapper = mount(AppFooter);
-    expect(wrapper.classes()).toContain('font-mono');
-    expect(wrapper.classes()).toContain('font-normal');
-    expect(wrapper.classes()).toContain('text-xs');
+    expect(wrapper.text()).toContain(currentYear.toString());
   });
 });
