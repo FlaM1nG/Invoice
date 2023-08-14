c50a07099e9325e7bdd92f4f836504e4139c3855
flam1ng <carlosamg87@gmail.com>
2023-08-14 21:40:10 +0200

    test(appheader): refactor test

--- src/components/__tests__/AppHeader.spec.ts
+++ src/components/__tests__/AppHeader.spec.ts
@@ -1,41 +1,63 @@
-import { describe, it, expect } from 'vitest'
-import { createRouter, createMemoryHistory } from 'vue-router'
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { createRouter, createMemoryHistory } from 'vue-router';
+import AppHeader from '@/components/AppHeader.vue';
+import HomeView from '@/views/HomeView.vue';
+import LoginView from '@/views/LoginView.vue';
+import AppIcon from '@/components/AppIcon.vue'; 
+import IconInvoice from '@/components/icons/IconInvoice.vue'; 
 
-import { mount } from '@vue/test-utils'
-import AppHeader from '@/components/AppHeader.vue'
-import HomeView from '@/views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
+describe('AppHeader', () => {
+  it('renders correctly on home route', async () => {
+    const routes = [
+      { path: '/', name: 'home', component: HomeView },
+      { path: '/login', name: 'login', component: LoginView },
+    ];
 
-const routes = [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/login', name: 'login', component: LoginView }
-  ];
+    const router = createRouter({
+      history: createMemoryHistory(),
+      routes,
+    });
 
-const router = createRouter({
-    history: createMemoryHistory(),
-    routes,
+    const wrapper = mount(AppHeader, {
+      global: {
+        components: {
+          AppIcon,
+          IconInvoice,
+        },
+        plugins: [router],
+        mocks: {
+          $t: (key: any) => key, // Mock the translation function
+        },
+      },
+    });
+
+    router.push('/');
+    await router.isReady();
+
+    const routerLink = wrapper.findComponent({ name: 'RouterLink' });
+    expect(routerLink.exists()).toBe(true);
+
+    router.push('/'); 
+    await router.isReady();
   });
 
-describe('Header', () => {
-    it('renders correctly when not on login route', () => {
-        const wrapper = mount(AppHeader, {
-            global: {
-              mocks: {
-                $route: {
-                  name: 'home'
-                }
-              },
-              plugins: [router], 
-            }
-          });
-    
-        const header = wrapper.find('header');
-        expect(header.exists()).toBe(true);
-    
-        const nav = wrapper.find('nav');
-        expect(nav.exists()).toBe(true);
-    
-        const routerLink = wrapper.findComponent({ name: 'RouterLink' });
-        expect(routerLink.exists()).toBe(true);
-      });
+  it('renders correctly on login route', async () => {
+    const routes = [
+      { path: '/', name: 'home', component: HomeView },
+      { path: '/login', name: 'login', component: LoginView },
+    ];
+
+    const router = createRouter({
+      history: createMemoryHistory(),
+      routes,
     });
+
+
+    router.push('/login');
+    await router.isReady();
+
+    router.push('/');
+    await router.isReady();
+  });
+});
