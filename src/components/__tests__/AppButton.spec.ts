0211416a8cb642d0293e3c29e047dd7b8b2f6a51
flam1ng <carlosamg87@gmail.com>
2023-08-14 21:39:20 +0200

    test(appbutton): refactor test

--- src/components/__tests__/AppButton.spec.ts
+++ src/components/__tests__/AppButton.spec.ts
@@ -1,33 +1,40 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import AppButton from '@/components/AppButton.vue';
 
-import { mount } from '@vue/test-utils'
-import AppButton from '@/components/AppButton.vue'
+describe('CustomButton', () => {
+  it('renders correctly when enabled', () => {
+    const wrapper = mount(AppButton, {
+      props: {
+        isDisabled: false,
+      },
+    });
 
-describe('AppButton', () => {
-    it('renders button correctly', () => {
-      const wrapper = mount(AppButton)
-      expect(wrapper.exists()).toBe(true)
-    })
-    it('emits custom-click event when clicked', async () => {
-      const wrapper = mount(AppButton)
-      await wrapper.trigger('click')
-      expect(wrapper.emitted()['custom-click']).toBeTruthy()
-    })
-    it('disables button when isDisabled prop is true', () => {
-      const wrapper = mount(AppButton, {
-        props: {
-          isDisabled: true,
-        },
-      })
-      expect(wrapper.find('button').element.disabled).toBe(true)
-    })
-    it('applies custom background color class when bgColorText prop is provided', () => {
-      const bgColor = 'bg-custom-color'
-      const wrapper = mount(AppButton, {
-        props: {
-          bgColorText: bgColor,
-        },
-      })
-      expect(wrapper.find('button').classes()).toContain(bgColor)
-    })
-})
+    expect(wrapper.find('button').exists()).toBe(true);
+    expect(wrapper.find('.btn').exists()).toBe(true);
+  });
+
+  it('renders correctly when disabled', () => {
+    const wrapper = mount(AppButton, {
+      props: {
+        isDisabled: true,
+      },
+    });
+
+    expect(wrapper.find('button').exists()).toBe(true);
+    expect(wrapper.find('.btn').exists()).toBe(true);
+  });
+
+  it('applies background color class', () => {
+    const bgColorText = 'bg-red-500';
+    const wrapper = mount(AppButton, {
+      props: {
+        isDisabled: false,
+        bgColorText,
+      },
+    });
+
+    expect(wrapper.find('.btn').classes()).toContain(bgColorText);
+  });
+
+});
