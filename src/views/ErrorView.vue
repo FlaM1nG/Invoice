aaf94bb06cda3fb7708882b580cad8c1acd6d551
flam1ng <carlosamg87@gmail.com>
2023-08-14 14:46:14 +0200

    feat(errorview): languages

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex-col container mx-auto px-5 sm:px-20 py-20 flex justify-center">
-      <h1 class="text-gray-700 text-6xl text-center"> Page not found.</h1>
+      <h1 class="text-gray-700 text-6xl text-center"> {{ $t('error.page') }}</h1>
       <p class="text-gray-600 text-8xl text-center mt-8">
-        Error 404
+        {{ $t('error.404') }}
       </p>
       <router-link
         to="/home"
         class="mt-5 text-xl text-green-600 hover:underline text-center">
-          Back to the home
+        {{ $t('error.back') }}
       </router-link>
     </div>
 </template>
