2dda970ed48d4cd5c355cfd4947f2e87ecf5e5d5
flam1ng <carlosamg87@gmail.com>
2023-08-14 21:41:14 +0200

    test(appstepper): new test stepper

---
+++ src/components/__tests__/AppStepper.spec.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import AppStepper from '@/components/AppStepper.vue';
+
+describe('Stepper', () => {
+  it('renders correctly with default props', () => {
+    const wrapper = mount(AppStepper, {
+      props: {
+        step: 1,
+        totalStep: 3,
+      },
+    });
+
+    expect(wrapper.find('.wrapper-stepper').exists()).toBe(true);
+    expect(wrapper.findAll('.stepper-item').length).toBe(3);
+  });
+
+  it('applies current and success classes based on step', () => {
+    const wrapper = mount(AppStepper, {
+      props: {
+        step: 2,
+        totalStep: 4,
+      },
+    });
+
+    const stepperItems = wrapper.findAll('.stepper-item');
+    expect(stepperItems[0].classes()).toContain('success');
+    expect(stepperItems[1].classes()).toContain('current');
+  });
+});
