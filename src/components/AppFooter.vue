c0ff302a6fff722fd5c5373f19ec20d43cc4c589
flam1ng <carlosamg87@gmail.com>
2023-08-14 14:46:35 +0200

    feat(appfooter): languages

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="w-full text-black font-mono font-normal items-center inline-flex justify-center h-10 text-xs bg-gradient-to-r from-teal-200 to-yellow-200">
-        Invoice
-        Copyright © {{currentYear }} · All Rights Reserved
+      {{ $t('invoice') }}
+        Copyright © {{currentYear }} ·{{ $t('footer.reserved') }}
     </footer> 
  </template>
  
